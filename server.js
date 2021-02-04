0b48ceed14273ffe1345808c7ec3083ab1350d2f
iballi2017 <iballi2017@gmail.com>
2021-02-04 16:30:18 +0100

    first commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/task-management-app__simple-web-application__frontend-software__angular-11'));
+app.use(express.static('./dist/ng11-taskprogressApp-heroku-deploy'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/task-management-app__simple-web-application__frontend-software__angular-11/'}),
+    res.sendFile('index.html', {root: 'dist/ng11-taskprogressApp-heroku-deploy/'}),
 );
 
 // Start the app by listening on the default Heroku port
